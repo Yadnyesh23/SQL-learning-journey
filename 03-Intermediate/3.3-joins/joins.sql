b6c4058ea9736e972be4285221e6c2a011f684fd
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-05-03 20:00:47 +0530

    Joins

--- 03-Intermediate/3.3-joins/joins.sql
+++ 03-Intermediate/3.3-joins/joins.sql
@@ -8,6 +8,8 @@ SELECT table1.column1 , table2.column2 FROM table1 INNER JOIN table2 ON table1.i
 -- LEFT JOIN
 SELECT table1.column1 , table2.column2 FROM table1 INNER JOIN table2 ON table1.id = table2.id
 
+-- RIGHT JOIN
+SELECT table1.column1 , table2.column2 FROM table1 RIGHT JOIN table2 ON table1.id = table2.id
 
 -- Example
 
@@ -17,3 +19,6 @@ SELECT users.name, addresses.city FROM users INNER JOIN addresses ON users.id = 
 
 -- LEFT JOIN
 SELECT users.name, addresses.city FROM users LEFT JOIN addresses ON users.id = addresses.user_id
+
+-- RIGHT JOIN
+SELECT users.name, addresses.city FROM users RIGHT JOIN addresses ON users.id = addresses.user_id
